31bc83f47e4be43421b89520ae01e04846f45824
Olga_N <nazar.olgaa@gmail.com>
2024-06-29 17:38:59 -0400

    CRUD List in svelte added

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,10 +1,10 @@
 <script>
-	export let name;
+	import CrudList from './components/CrudList.svelte';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>CRUD List</h1>
+	<CrudList />
 </main>
 
 <style>
